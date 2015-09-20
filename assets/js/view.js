3f539cf22f8a407e4366e3f4545f4c8cb02821bf
Makis Tracend <makis.tracend@gmail.com>
2015-09-19 20:03:22 -0700

    core logic

--- assets/js/view.js
+++ assets/js/view.js
@@ -4,6 +4,67 @@
 
 	var View = APP.View.extend({
 
+		events: {
+			"animationend": "onAnimationEnd",
+			"webkitAnimationEnd": "onAnimationEnd",
+			"MSAnimationEnd": "onAnimationEnd",
+			"oAnimationEnd": "onAnimationEnd"
+		},
+
+		initialize: function( options ){
+			//console.log("options", options);
+			//this.setup();
+			this.render();
+			return APP.View.prototype.initialize.call(this, options);
+		},
+
+		setup: function(){
+			// check if we need to render...
+			this.render();
+		},
+		update: function(){
+			// check if we need to render...
+			this.render();
+		},
+
+		setup: function(){
+			var self = this,
+				$el = this.$el;
+
+			$el.on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function(){
+			//$el.on('webkitTransitionEnd oTransitionEnd msTransitionEnd transitionend', function(){
+
+			});
+		},
+
+		render: function(){
+			// mirror delay / duration...
+			this.$el.css({
+				"-webkit-animation-duration": this.options.duration +"s",
+				"-moz-animation-duration": this.options.duration +"s",
+				"-o-animation-duration": this.options.duration +"s",
+				"animation-duration": this.options.duration +"s",
+				"-webkit-animation-delay": this.options.delay +"s",
+				"-moz-animation-delay": this.options.delay +"s",
+				"-o-animation-delay": this.options.delay +"s",
+				"animation-delay": this.options.delay +"s"
+			});
+			// update animation class
+			if( this.options.animation == "fade" ) this.$el.removeClass("pulse").addClass("fade");
+			if( this.options.animation == "pulse" ) this.$el.removeClass("fade").addClass("pulse");
+		},
+
+		onAnimationEnd: function(){
+			if( this.options.animation == "fade"){
+				// remove all contents
+				this.$el.empty();
+				// unbind events
+				this.$el.unbind();
+				// remove from the page
+				this.remove();
+			}
+		}
+
 	});
 
 	APP.UI = APP.UI || {};
--- src/main.js
+++ src/main.js
@@ -5,27 +5,44 @@
 	// Fires when an instance of the element is created
 	el.createdCallback = function() {
 
-		// gather options
-		var options = {};
-		// ...
-		options.el = this;
-		// instantiate view
-		this.view = new APP.UI.Component( options );
-
 	};
 
 	// Fires when an instance was inserted into the document
-	el.attachedCallback = function() {};
+	el.attachedCallback = function() {
+
+		// gather attributes
+		var animation = this.getAttribute("animation");
+		var delay = this.getAttribute("delay");
+		var duration = this.getAttribute("duration");
+
+		// gather options
+		var options = {
+			animation: animation || "static", // options: static, pulse, fade, random
+			delay: delay || 0,
+			duration: duration || 0
+		};
+		// ...
+		options.el = this;
+		// instantiate view
+		this.view = new APP.UI.Overlay( options );
+
+	};
 
 	// Fires when an instance was removed from the document
 	el.detachedCallback = function() {
-		this.view.destroy();
+		if( this.view ) this.view.remove();
 	};
 
 	// Fires when an attribute was added, removed, or updated
-	el.attributeChangedCallback = function(attr, oldVal, newVal) {};
+	el.attributeChangedCallback = function(attr, oldVal, newVal) {
+		// prerequisite(s)
+		if(!this.view) return;
+		// filter options?
+		this.view.options[attr] = newVal;
+		this.view.update();
+	};
 
-	document.registerElement('ui-component', {
+	document.registerElement('ui-overlay', {
 		prototype: el
 	});
 }());
